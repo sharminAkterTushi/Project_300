3c0b4fb937d499e2d57e4db1740ba66b52a2f487
sharminAkterTushi <sharmin.tushi95@gmail.com>
2020-11-23 22:39:35 +0600

    Project_300

---
+++ application/views/pages/about.php
@@ -0,0 +1,8 @@
+<h2> <?php echo $title; ?></h2>
+<center>
+<img height="250px" width="200px" src="<?php echo base_url('assets/images/posts/noimage.jpg') ?>">
+<h2>Campus Correspondent,BTV</h2>
+</center>
+<center>
+<p style="color: green;font-size: 18px"> We will best try to give you our campus Latest update, Please stay with us, we have a team of jurnalist on overall campus. All department we have different Correspondent of different news paper or television. Our mission is to achive a goal of white news from student life. We are againest yellow media. </p>
+</center>
---
+++ application/views/pages/home.php
@@ -0,0 +1,5 @@
+<h2> <?php echo $title; ?></h2>
+<p>This Blog is a news portal of a Jusnalist</p>
+<p>Correspondent of BTV,Metropolitan University </b>
+<a >Sharmin Akter Tushi </a>
+</p>
